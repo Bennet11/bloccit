0b805ccbdb27b6be142a1bce88a38df5ac8dced6
Bennet11 <bennetveloya@gmail.com>
2016-05-08 22:34:18 -0700

    assignment_34 migration problem

--- db/migrate/20160508232151_add_topicto_sponsored_posts.rb
+++
@@ -1,4 +0,0 @@
-class AddTopictoSponsoredPosts < ActiveRecord::Migration
-  def change
-  end
-end
--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -7,51 +7,51 @@ RSpec.describe SponsoredPostsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      # get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
+      get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
       expect(response).to have_http_status(:success)
     end
 
     it "renders the #show view" do
-      # get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
+      get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
       expect(response).to render_template :show
     end
 
     it "assigns my_sponsored_post to @sponsoredpost" do
-      # get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
+      get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
       expect(assigns(:sponsoredpost)).to eq(my_sponsoredpost)
     end
   end
 
   describe "GET #new" do
     it "returns http success" do
-      # get :new, topic_id: my_topic.id
+      get :new, topic_id: my_topic.id
       expect(response).to have_http_status(:success)
     end
 
     it "renders #new view" do
-      # get :new, topic_id: my_topic.id
+      get :new, topic_id: my_topic.id
       expect(response).to render_template :new
     end
 
     it "initializes @sponsoredpost" do
-      # get :new, topic_id: my_topic.id
+      get :new, topic_id: my_topic.id
       expect(assigns(:sponsoredpost)).not_to be_nil
     end
   end
 
   describe "GET #edit" do
     it "returns http success" do
-      # get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
+      get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
       expect(response).to have_http_status(:success)
     end
 
     it "renders #edit view" do
-      # get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
+      get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
       expect(response).to render_template :edit
     end
 
     it "assigns sponsoredpost to be updated at @sponsoredpost" do
-      # get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
+      get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
       sponsoredpost_instance = assigns(:sponsoredpost)
 
       expect(sponsoredpost_instance.id).to eq my_sponsoredpost.id
