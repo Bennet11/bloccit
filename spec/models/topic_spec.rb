7eea474242f83605459e9e962f55cd2675facaab
Charlie Gaines <beaugaines@gmail.com>
2016-05-11 18:23:36 -0400

    specs green

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe Topic, type: :model do
   let(:public) {true}
   let(:topic) {Topic.create!(name: name, description: description)}
 
-  it { is_expected.to have_many(:SponsoredPost) }
+  it { is_expected.to have_many(:sponsored_posts) }
   it { is_expected.to have_many(:posts) }
+
   describe "attributes" do
     it "has name, description, public attributes" do
       expect(topic).to have_attributes(name: name, description: description, public: public)
